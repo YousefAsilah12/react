006a4d6ebd4dab0eda1062c8e46d5ef70ab38248
Yous3f <yasilah4@gmail.com>
2023-02-19 14:44:06 +0200

    10

--- 10/10.2/src/image.jsx
+++ 10/10.2/src/image.jsx
@@ -1,30 +1,32 @@
-
-
-import port1 from'./assets/photos/port1.png';
-import port2 from'./assets/photos/port2.png';
-import port1Color from'./assets/photos/port1-color.jpg';
-import port2Color from'./assets/photos/port2-color.jpg';
+import React from 'react';
+import port1 from './assets/photos/port1.png';
+import port2 from './assets/photos/port2.png';
+import port1Color from './assets/photos/port1-color.jpg';
+import port2Color from './assets/photos/port2-color.jpg';
 import { useRef } from 'react';
 
+export const Image = () => {
+  const images = [port1, port1Color, port2, port2Color];
+  const imageRefs = useRef(Array.from({ length: images.length }, () => React.createRef()));
 
+  const handleMouseOver = (index) => {
+    imageRefs.current[index].current.src = images[index + 1];
+  };
+  const handleMouseOut = (index) => {
+    imageRefs.current[index].current.src = images[index];
+  };
 
-
-function mouseOverHandle1(){
-  refImg1.current.src=port1Color
+  return images.map((image, index) => {
+    if(index %2!==0){
+      return
+    }
+    return <img
+      key={index}
+      ref={imageRefs.current[index]}
+      src={image}
+      alt={`Image ${index + 1}`}
+      onMouseOver={() => handleMouseOver(index)}
+      onMouseOut={() => handleMouseOut(index)}
+    />
+  });
 }
-
-function mouseOverHandle2(){
-  refImg2.current.src=port2Color
-}
-export const Image=()=>{
-  const refImg1=useRef(null);
-  const refImg2=useRef(null);
-
-  refImg1.current.src=port1;
-  // refImg2.current.src=port2;
-
-  return <div style={{display:"flex" ,gap:"2rem"}}>
-    <img  src={port1}    alt="" />
-    {/* <img   onMouseOver={mouseOverHandle2} alt="" /> */}
-  </div>
- }
